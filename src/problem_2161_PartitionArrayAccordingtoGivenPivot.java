73d1fc5558b8998190f1271a0e8629cc91b42832
ad-aditya01 <adityaazm001@gmail.com>
2026-06-08 17:55:21 +0530

    leetcode problem 2161

--- src/problem_2161_PartitionArrayAccordingtoGivenPivot.java
+++ src/problem_2161_PartitionArrayAccordingtoGivenPivot.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class problem_2161_PartitionArrayAccordingtoGivenPivot {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int[] result=new int[nums.length];
+        int idx=0;
+        for(int num:nums){
+            if(num<pivot){
+                result[idx]=num;
+                idx++;
+            }
+        }
+        for(int num:nums){
+            if(num==pivot){
+                result[idx]=num;
+                idx++;
+            }
+        }
+        for(int num:nums){
+            if(num>pivot){
+                result[idx]=num;
+                idx++;
+            }
+        }
+        return result;
+    }
 }
