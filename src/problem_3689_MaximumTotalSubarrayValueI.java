ae26ee07d23055c7006ab6a4ac6ce6260949cbed
ad-aditya01 <adityaazm001@gmail.com>
2026-06-09 19:49:32 +0530

    leetcode problem no.3689

---
+++ src/problem_3689_MaximumTotalSubarrayValueI.java
@@ -0,0 +1,12 @@
+public class problem_3689_MaximumTotalSubarrayValueI {
+    public long maxTotalValue(int[] nums, int k) {
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        for(int num:nums){
+            min=Math.min(min,num);
+            max=Math.max(max,num);
+        }
+        return (long)(max-min)*k;
+
+    }
+}
