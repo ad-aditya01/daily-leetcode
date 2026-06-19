c871297c36b270df538b613e549e1ed30436c5e7
ad-aditya01 <adityaazm001@gmail.com>
2026-06-19 09:41:29 +0530

    leetcode problem no 1732

---
+++ src/problem_1732_FindtheHighestAltitude.java
@@ -0,0 +1,14 @@
+public class problem_1732_FindtheHighestAltitude {
+    public int largestAltitude(int[] gain) {
+        int sum=0;
+        int max=0;
+        for(int i=0;i<gain.length;i++){
+            sum=sum+gain[i];
+            if(sum>max){
+                max=sum;
+            }
+        }
+        return max;
+
+    }
+}
